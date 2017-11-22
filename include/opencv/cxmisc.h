3159a834402cdeeaa704ae7c93a661c63d2a10b4
rwismeth@hm.edu <kontakt@robin-wismeth.de>
2017-11-22 20:25:00 +0100

    change opencv version

--- include/opencv/cxmisc.h
+++ include/opencv/cxmisc.h
@@ -1,8 +1,6 @@
-#ifndef OPENCV_OLD_CXMISC_H
-#define OPENCV_OLD_CXMISC_H
+#ifndef __OPENCV_OLD_CXMISC_H__
+#define __OPENCV_OLD_CXMISC_H__
 
-#ifdef __cplusplus
-#  include "opencv2/core/utility.hpp"
-#endif
+#include "opencv2/core/internal.hpp"
 
 #endif
